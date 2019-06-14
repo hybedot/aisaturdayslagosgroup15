d55723356909789520bb43e2aafab19d97616fb2
hybedot <aisaturdayslagosgroup15@gmail.com>
2019-06-14 13:09:35 +0100

    testing 4

--- app.py
+++ app.py
@@ -19,32 +19,32 @@ class Predict(Resource):
     def post(self):
         json_data = request.get_json()
         
-        #try:
-        age = json_data['age']
-        position_category = json_data['positionCategory'] 
-        signing_category = json_data['signingCategory']
-        page_views = json_data['pageViews']
-        fpl_value = json_data['fplValue']
-        fpl_sell = json_data['fplSell']
-        fpl_points = json_data['fplPoints']
-        big_club_category = json_data['bigClubCategory']
+        try:
+            age = json_data['age']
+            position_category = json_data['positionCategory'] 
+            signing_category = json_data['signingCategory']
+            page_views = json_data['pageViews']
+            fpl_value = json_data['fplValue']
+            fpl_sell = json_data['fplSell']
+            fpl_points = json_data['fplPoints']
+            big_club_category = json_data['bigClubCategory']
 
-        prediction_data = [age, position_category, signing_category, page_views, fpl_value, fpl_sell, fpl_points, big_club_category]
+            prediction_data = [age, position_category, signing_category, page_views, fpl_value, fpl_sell, fpl_points, big_club_category]
 
-        prediction = np.array2string(model.predict([prediction_data]))
+            prediction = np.array2string(model.predict([prediction_data]))
 
-        prediction = prediction.strip('[]')
+            prediction = prediction.strip('[]')
 
 
-        return {
-                'status code':'200',
-                'message':prediction
-                }, 200
-        # except:
-        #     return {
-        #             'status code':'500',
-        #             'message':'Invalid Values'
-        #             }, 500
+            return {
+                    'status code':'200',
+                    'message':prediction
+                    }, 200
+        except:
+            return {
+                    'status code':'500',
+                    'message':'Invalid Values'
+                    }, 500
 
 
 
